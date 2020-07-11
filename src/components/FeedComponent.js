e29c119fdbeb0c25d0ae8b10968289654625f2f5
Felix Anducho <felix.anducho74@gmail.com>
2020-07-10 19:42:57 -0500

    Add #menu-toggler in main.css to open navbar

--- src/components/FeedComponent.js
+++ src/components/FeedComponent.js
@@ -12,7 +12,7 @@ const FeedComponent = () => {
 
     return (
         allPosts.map(post => 
-        <div key={post.id} className="max-w-sm rounded overflow-hidden shadow-lg container mx-auto">
+        <div key={post.id} className="max-w-sm rounded overflow-hidden shadow-lg flex flex-col container mx-auto py-3">
           <h2 className="text-3xl px-2 font-bold">{post.title}</h2>
           <p className="px-2">{post.id}</p>
           <img src={baseUrl+post.postImage} className="px-2"></img>
