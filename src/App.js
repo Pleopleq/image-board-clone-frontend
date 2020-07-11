aa3547ae303733ac2d6f6c33fe6d58b18db77683
Felix Anducho <felix.anducho74@gmail.com>
2020-07-10 19:35:59 -0500

    Installed tailwindcss. Add responsive-navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import FeedComponent from './components/FeedComponent'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
+
     <div className="App">
+        <Navbar></Navbar>
         <FeedComponent></FeedComponent>
     </div>
   );
--- src/components/FeedComponent.js
+++ src/components/FeedComponent.js
@@ -12,17 +12,19 @@ const FeedComponent = () => {
 
     return (
         allPosts.map(post => 
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.id}</p>
-          <img src={baseUrl+post.postImage}></img>
-          <p>author: {post.author}</p>
-          <p>{post.content}</p>
+        <div key={post.id} className="max-w-sm rounded overflow-hidden shadow-lg container mx-auto">
+          <h2 className="text-3xl px-2 font-bold">{post.title}</h2>
+          <p className="px-2">{post.id}</p>
+          <img src={baseUrl+post.postImage} className="px-2"></img>
+          <p className="font-bold text-purple-500 text-xl-mb-2 px-2">Author: {post.author}</p>
+          <p className="py-3 px-2">{post.content}</p>
           <div>
-              <h3>Comments</h3>
-          <ul>
+              <h3 className="text-2xl font-bold px-4">Comments</h3>
+          <ul className="px-4">
               {post.replies.map((comment, index) => 
-              <li key={index}>{comment.message} - {comment.author}</li>
+                <li key={index} className="bg-gray-200font-semibold text-gray-700 py-2">
+                  {comment.message} - {comment.author}
+                </li>
               )}
           </ul>
           </div>
